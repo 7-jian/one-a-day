3cd7b7937163d82e516b4fbbe635fb78d4fcde60
7jian <1420274699@qq.com>
2022-12-21 17:29:07 +0800

    add all exercises

--- vue/vue3-mini-project/src/pages/home.vue
+++ vue/vue3-mini-project/src/pages/home.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>这是首页</h1>
+  <TodoList />
 </template>
+
+<script setup lang="ts">
+import TodoList from '../components/TodoList.vue'
+import {useMouse} from '../utils/mouse'
+
+let {x,y} = useMouse()
+</script>
